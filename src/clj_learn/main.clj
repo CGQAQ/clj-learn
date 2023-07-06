434ad822829e44e46d57ae2e1e8e14a623e57cff
CGQAQ <m.jason.liu@outlook.com>
2023-07-06 02:51:37 +0000

    defn, construct java object, call method

--- src/clj_learn/main.clj
+++ src/clj_learn/main.clj
@@ -1,4 +1,17 @@
 (ns clj-learn.main)
 
+(import java.util.Date)
+
+(defn add [a b]
+  (+ a b))
+
+
+;;  (def my-date (java.util.Date.))
+
+
 (defn -main []
-  (println "hello world"))
+  (println "hello world")
+  (println (add 5 6))
+
+  (let [my-date (java.util.Date.)]
+    (println (.toString my-date))))
